29aedb0248e239757340ece749933383ff5ff4fe
CharlesStanski <90924992+CharlesStanski@users.noreply.github.com>
2022-03-18 14:02:23 -0400

    updated user

--- js/removePast.js
+++ js/removePast.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 var ObjectID = require('mongodb').ObjectID;
-let removePast = ["class28"] //Class you want to remove
-var email = "testFind&Update@students.rowan.edu"
+let removePast = ["L6101CHEM"] //Class you want to remove
+var email = "chuck@rowan.students.edu"
 
 async function main(){
     const uri = "";//insert mongodb connection string
@@ -37,7 +37,7 @@ async function main(){
                         }
                         else
                         {
-                            oldPast[i] = "Can't be Red"
+                            oldPast[i] = ""
                             k = i
         
                             while(k < oldPast.length - 1)
@@ -83,16 +83,16 @@ main().catch(console.error);
 
 async function findUser(client, newUser){  //find document with specified email
 
-    const result = await client.db("saction-tally1").collection("user").findOne(newUser);
+    const result = await client.db("section_tally_plus").collection("user").findOne(newUser);
     oldPast = result.past_classes;
     return oldPast;  //return the current past_classes array of classes
     
 }
 
 async function removePastClass(client, email, oldPast){  //update document with new past_classes array
-    const result = await client.db("saction-tally1").collection("user").findOne(email);
+    const result = await client.db("section_tally_plus").collection("user").findOne(email);
     var q = { "_id": ObjectID(result._id)}
     var newValue = { $set: {"past_classes": oldPast } };
     console.log(email);
-    await client.db("saction-tally1").collection("user").updateOne(q, newValue);
+    await client.db("section_tally_plus").collection("user").updateOne(q, newValue);
 }
